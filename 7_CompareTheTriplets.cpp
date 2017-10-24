5f4992617e0d1a95ef326dd85b04ec8092054cfc
rich7021 <coconutfu@gmail.com>
2017-10-24 17:16:12 +0800

    refact compare the triplets

--- 7_CompareTheTriplets.cpp
+++ 7_CompareTheTriplets.cpp
@@ -17,20 +17,24 @@ vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
 }
 
 int main() {
-    int a0;
-    int a1;
-    int a2;
-    cin >> a0 >> a1 >> a2;
-    int b0;
-    int b1;
-    int b2;
-    cin >> b0 >> b1 >> b2;
-    vector < int > result = solve(a0, a1, a2, b0, b1, b2);
-    for (ssize_t i = 0; i < result.size(); i++) {
-        cout << result[i] << (i != result.size() - 1 ? " " : "");
-    }
-    cout << endl;
+    int a_score = 0;
+    int b_score = 0;
+    vector<int> a_trip(3);
+    vector<int> b_trip(3);
     
+    for(int i = 0 ; i < 3 ; i++) {
+        cin >> a_trip[i];
+    }
+    for(int i = 0 ; i < 3 ; i++) {
+        cin >> b_trip[i];
+    }
+    
+    for(int i = 0 ; i < a_trip.size() ; i++ ) {
+        if (a_trip[i] > b_trip[i]) { a_score++; }
+        if (a_trip[i] < b_trip[i]) { b_score++; }
+    }
+   
+    cout << a_score << " " << b_score ;
 
     return 0;
 }
