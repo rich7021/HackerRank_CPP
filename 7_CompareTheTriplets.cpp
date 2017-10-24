f0ac8721d182131da9d3097e8e12bc4562e50347
rich7021 <coconutfu@gmail.com>
2017-10-24 17:06:01 +0800

    finish compare the triplets using the gave code by hackerrank

---
+++ 7_CompareTheTriplets.cpp
@@ -0,0 +1,39 @@
+#include "ExcutionDefine.h"
+#ifdef RUN_COMPARE_THE_TRIPLETS
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
+    // Complete this function
+    vector<int> arr(2);
+    
+    if (a0 > b0) { arr[0]++; } else if (a0 < b0) { arr[1]++; }
+    if (a1 > b1) { arr[0]++; } else if (a1 < b1) { arr[1]++; }
+    if (a2 > b2) { arr[0]++; } else if (a2 < b2) { arr[1]++; }
+    
+    return arr;
+}
+
+int main() {
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    vector < int > result = solve(a0, a1, a2, b0, b1, b2);
+    for (ssize_t i = 0; i < result.size(); i++) {
+        cout << result[i] << (i != result.size() - 1 ? " " : "");
+    }
+    cout << endl;
+    
+
+    return 0;
+}
+
+
+#endif
